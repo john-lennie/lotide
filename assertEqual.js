44bccf987b3dfcb65b8ef44ef5ce44acacfff841
John Lennie <johnanthonylennie@gmail.com>
2019-10-10 16:02:30 -0400

    added first function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ ✅ ✅ assertion passed:", actual, "===", expected);
+  } else {
+    console.log("🛑 🛑 🛑 assertion failed:", actual, "!==", expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
